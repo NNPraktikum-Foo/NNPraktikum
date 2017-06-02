d29966fbafcf2fd9434e3b0330828215910b2d6c
Carsten <taxus13+github@lostbit.de>
2017-06-02 16:17:58 +0200

    implement activation function

--- src/util/activation_functions.py
+++ src/util/activation_functions.py
@@ -19,12 +19,12 @@ class Activation:
 
     @staticmethod
     def sigmoid(netOutput):
-        pass
+        return 1 / (1 + exp(-netOutput))
+
     @staticmethod
     def sigmoidPrime(netOutput):
         # Here you have to code the derivative of sigmoid function
-        # netOutput.*(1-netOutput)
-        pass
+        return netOutput * (1 - netOutput)
 
     @staticmethod
     def tanh(netOutput):
