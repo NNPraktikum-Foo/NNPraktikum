a578d994cd3050090a0bad4cbd13963b2c97c855
henry0711 <enrico93@online.de>
2017-06-11 14:42:34 +0200

    Added plottings
    Minor improvements to code style. Changed from MSE to RMSE

--- src/model/logistic_regression.py
+++ src/model/logistic_regression.py
@@ -4,8 +4,12 @@ import sys
 import logging
 
 import numpy as np
+import matplotlib
+matplotlib.use('Qt4Agg')
+from matplotlib import pyplot as plt
 
 from util.activation_functions import Activation
+from util.loss_functions import RootMeanSquaredError
 from model.classifier import Classifier
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
@@ -35,7 +39,7 @@ class LogisticRegression(Classifier):
     epochs : positive int
     """
 
-    def __init__(self, train, valid, test, learningRate=0.01, epochs=50):
+    def __init__(self, train, valid, test, learningRate=0.01, epochs=50, min_rmse=0.0, max_accuracy=100):
 
         self.learningRate = learningRate
         self.epochs = epochs
@@ -46,6 +50,8 @@ class LogisticRegression(Classifier):
 
         # Initialize the weight vector with small values
         self.weight = 0.01*np.random.randn(self.trainingSet.input.shape[1])
+        self.min_rmse = min_rmse
+        self.max_accuracy = max_accuracy
 
     def train(self, verbose=True):
         """Train the Logistic Regression.
@@ -61,41 +67,48 @@ class LogisticRegression(Classifier):
         #Decay constants
         base = 0.96
         decay_interval = 100
-        #Stopping constants
-        min_q_error = 0.0
-        max_accuracy = 100 #not implemented yet
-        min_improvement = 1.0 #not implemented yet
+        plot_interval = 100
 
         #Input preprocessing
         input = self.trainingSet.input
         label = np.array(self.trainingSet.label)
         size = input.shape[0]
         positives = np.count_nonzero(label)
-      	#Average number of negatives for each positive
-        inbalance = float(size)/positives - 1
+        #Plotting
+        rmse = list()
+        acu = list()
 
         #Training
         while not stopping:
-        		misses = 0
-        		fired = self.fire(input)
-        		signed_error = label - fired
-        		for i in range(size):
-        				if abs(signed_error[i]) >= 0.5:
-        						misses += 1
-        		
-        		grad = np.dot(signed_error, input)
-        		self.updateWeights(grad)
-        		e += 1
-        		quad_error = np.average(np.power(signed_error, 2))
-        		if (e%decay_interval == 0):
-        				self.decayLearningRate(base)
-        		if verbose and (e%100 == 0):
-        				accuracy = (size - misses)*100.0/size
-        				logging.info("Epoch: %i; Error: %f; Misses: %i, Acc: %f",
-        				 e, quad_error, misses, accuracy)
-        		if e >= self.epochs or quad_error < min_q_error:
-        				stopping = True
-        				logging.info("Final learningRate: %f", self.learningRate)
+            misses = 0
+            fired = self.fire(input)
+            signed_error = label - fired
+            
+            grad = np.dot(signed_error, input)
+            self.updateWeights(grad)
+            e += 1
+
+            #Decay learning rate
+            if (e%decay_interval == 0):
+                self.decayLearningRate(base)
+
+            #Calculate ratings
+            misses = np.sum(abs(signed_error) >= 0.5)
+            curr_rmse = RootMeanSquaredError.calculateError(label, fired)
+            rmse.append(curr_rmse)
+            accuracy = (size - misses)*100.0/size
+            acu.append(accuracy)
+
+            #Logging
+            if verbose and (e%plot_interval == 0):
+                logging.info("Epoch: %i; Error: %f; Misses: %i; Acc: %f; LR: %f", e, curr_rmse, misses, accuracy, self.learningRate)
+            
+            #Is any stopping criterion fullfilled?
+            if self.stopping(e, rmse):
+                stopping = True
+                logging.info("Final learningRate: %f", self.learningRate)
+
+        self.plot(acu, rmse)
         
     def classify(self, testInstance):
         """Classify a single instance.
@@ -140,3 +153,19 @@ class LogisticRegression(Classifier):
 
     def decayLearningRate(self, base):
     		self.learningRate *= base
+
+    def plot(self, accuracy, rmse):
+        f, (ax1, ax2) = plt.subplots(1, 2)
+        ax1.plot(accuracy)
+        ax1.set_xlabel('Epoch')
+        ax1.set_ylabel('Accuracy')
+        ax1.set_ylim(97.5, 100)
+        ax2.plot(rmse)
+        ax2.set_xlabel('Epoch')
+        ax2.set_ylabel('RMSE')
+        ax2.set_ylim(0.0, 0.2)
+        plt.show()
+
+    def stopping(self, epoch, rmse=float("inf")):
+        return (epoch >= self.epochs) or (rmse < self.min_rmse)
+
--- src/util/loss_functions.py
+++ src/util/loss_functions.py
@@ -21,7 +21,7 @@ class Error:
         pass
 
     @abstractmethod
-    def calculateError(self, target, output):
+    def calculateError(target, output):
         # calculate the error between target and output
         pass
 
@@ -33,7 +33,8 @@ class AbsoluteError(Error):
     def errorString(self):
         self.errorString = 'absolute'
 
-    def calculateError(self, target, output):
+    @staticmethod
+    def calculateError(target, output):
         # It is the numbers of differences between target and output
         return abs(target - output)
 
@@ -45,7 +46,8 @@ class DifferentError(Error):
     def errorString(self):
         self.errorString = 'different'
 
-    def calculateError(self, target, output):
+    @staticmethod
+    def calculateError(target, output):
         # It is the numbers of differences between target and output
         return target - output
 
@@ -58,9 +60,24 @@ class MeanSquaredError(Error):
     def errorString(self):
         self.errorString = 'mse'
 
-    def calculateError(self, target, output):
+    @staticmethod
+    def calculateError(target, output):
         # MSE = 1/n*sum (i=1 to n) of (target_i - output_i)^2)
         return np.average((target-output)**2)
+
+
+class RootMeanSquaredError(Error):
+    """
+    The Loss calculated by the root of the mean of the total squares of differences
+    between target and output.
+    """
+    def errorString(self):
+        self.errorString = 'mse'
+
+    @staticmethod
+    def calculateError(target, output):
+        # RMSE = sqrt of 1/n*sum (i=1 to n) of (target_i - output_i)^2)
+        return np.sqrt(MeanSquaredError.calculateError(target, output))
 
 
 class SumSquaredError(Error):
@@ -71,7 +88,8 @@ class SumSquaredError(Error):
     def errorString(self):
         self.errorString = 'sse'
 
-    def calculateError(self, target, output):
+    @staticmethod
+    def calculateError(target, output):
         # SSE = 1/2*sum (i=1 to n) of (target_i - output_i)^2)
         
         return 0.5*np.sum((target-output)**2)
@@ -85,7 +103,8 @@ class BinaryCrossEntropyError(Error):
     def errorString(self):
         self.errorString = 'bce'
 
-    def calculateError(self, target, output):
+    @staticmethod
+    def calculateError(target, output):
         pass
 
 
@@ -97,5 +116,6 @@ class CrossEntropyError(Error):
     def errorString(self):
         self.errorString = 'crossentropy'
 
+    @staticmethod
     def calculateError(self, target, output):
         pass
